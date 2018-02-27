795d8ed6edda0b9cd640297db9b90cfc3d222305
sglodek <sglodek@gmail.com>
2018-02-26 16:27:06 -0800

    Added doctests

--- docstrings/passwd_analyzer_w_docstrings.py
+++ docstrings/passwd_analyzer_w_docstrings.py
@@ -1,7 +1,16 @@
 def split_group_line(line: str) -> list:
+    """
+    >>> split_group_line("hello:world") #doctest: +NORMALIZE_WHITESPACE
+    ['hello', 'world']
+    """
     return line.split(":")
 
 def parse_groups(file_path='/etc/group') -> list:
+    """
+    >>> parse_groups("group") #doctest: +NORMALIZE_WHITESPACE
+    [['wireshark', 'x', '983', 'sglodek']] 
+    """
+
     with open(file_path, "r") as group_list:
         group_list = group_list.read().split("\n")
         group_list.pop()
@@ -9,10 +18,19 @@ def parse_groups(file_path='/etc/group') -> list:
         return group_list
 
 def is_user_account(user_data: list) -> bool:
+    """
+    >>> is_user_account([0,0,1001]) #doctest: +NORMALIZE_WHITESPACE
+    True
+
+    """
     if int(user_data[2]) >= 1000:
         return True
 
 def get_user_accounts(passwd_file='/etc/passwd') -> list:
+    """
+    >>> get_user_accounts("passwd") #doctest: +NORMALIZE_WHITESPACE
+    [['sglodek', 'x', '1002', '1002', '', '/home/sglodek', '/bin/bash']] 
+    """
     with open(passwd_file, "r") as user_list:
         user_list = user_list.read().split("\n")
         user_list.pop()
@@ -21,15 +39,25 @@ def get_user_accounts(passwd_file='/etc/passwd') -> list:
         return user_list
 
 def get_sup_groups(users: list, groups: list) -> list:
-        for user in users:
-            supp_groups = []
-            for group in groups:
-                if user[0] in group[3]:
-                    supp_groups.append(group[0])
-            user.append(supp_groups)
-        return users
+    """
+    >>> users = [['sglodek', 'x', '1002', '1002', '', '/home/sglodek', '/bin/bash']]
+    >>> groups = [['wireshark', 'x', '983', 'sglodek']]
+    >>> get_sup_groups(users, groups) #doctest: +NORMALIZE_WHITESPACE
+    [['sglodek', 'x', '1002', '1002', '', '/home/sglodek', '/bin/bash', ['wireshark']]]
+    """
+    for user in users:
+        supp_groups = []
+        for group in groups:
+             if user[0] in group[3]:
+                 supp_groups.append(group[0])
+        user.append(supp_groups)
+    return users
 
 def gen_user_report(users: list, output_file='user_report.txt'):
+    """
+    >>> users = [['sglodek', 'x', '1002', '1002', '', '/home/sglodek', '/bin/bash', ['wireshark']]]
+    >>> gen_user_report(users) #doctest: +NORMALIZE_WHITESPACE
+    """
     with open(output_file, 'w') as output:
         for i in range(0, len(users)):
             if int(users[i][2]) >= 1000:
