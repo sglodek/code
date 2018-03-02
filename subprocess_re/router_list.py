0b04000a2ce2157a9ff23f7c3061a03dad193fe8
sglodek <sglodek@gmail.com>
2018-03-01 17:10:05 -0800

    subprocessed

--- subprocess_re/router_list.py
+++ subprocess_re/router_list.py
@@ -1,6 +1,7 @@
 import re
+import subprocess
 
-tracert = ["""
+tracert2 = ["""
  1  142.232.221.254  1.891 ms  1.883 ms  1.928 ms
  2  142.232.24.123  0.944 ms  0.997 ms  0.993 ms
  3  142.232.38.94  0.438 ms  0.439 ms  0.431 ms
@@ -28,12 +29,17 @@ tracert = ["""
 11  209.85.243.9  5.268 ms  5.096 ms  5.131 ms
 12  216.58.216.174  5.279 ms  5.455 ms  5.439 ms
 """]
+addresses_to_check = ["8.8.8.8", "yahoo.com", "google.com", "osnews.com", "facebook.com"]
+tracert = []
+for address in addresses_to_check:
+    route = subprocess.run("traceroute -n " + address, universal_newlines=True, stdout=subprocess.PIPE, shell=True)
+    tracert.append(route.stdout.splitlines())
 
 router_dict = {}
 for route in tracert:
     previous = None
-    for hop in route.split("\n"):
-        if hop != "":
+    for hop in route:
+        if hop.startswith("traceroute") == False:
             hop_num = re.search(r'\d{1,2}',hop).group()
             matched = re.findall(r'\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}', hop)
             for match in matched:
@@ -51,3 +57,4 @@ for router in router_dict:
         previous: {}
         """.format(router, router_dict[router]["hop"], router_dict[router]["count"], router_dict[router]["previous"])
     print(rt)
+
